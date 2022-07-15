79a6abe5280041ee94187c641a5a15373047fd5e
unknown <mammedov.r39@gmail.com>
2022-07-15 12:00:49 +0400

    project added

---
+++ FirsApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int input;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter first number: ");
+                    input = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You must enter interger numbers!!!");
+                }
+            }
+            Console.Write("Enter second number: ");
+            int input2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(input + " + " + input2 + " = " + sum(input, input2));
+        }
+        public static int sum(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
